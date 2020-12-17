b1317305739775ef330334ba940ebed05d512cd5
maninhouse <maninhouse@protonmail.com>
2020-12-17 17:56:20 +0800

    Add visual_recognition.py and trying to use file chooser in views.py

--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from app import app
 from flask import render_template
+import tkinter as tk
+from tkinter import filedialog
+
+def open_file():
+    #放入要讀取照片的位置
+    root = tk.Tk()
+    root.withdraw()
+    image_url = filedialog.askopenfilename()  
+    root.destroy()
 
 @app.route('/')
 def index():
-    return render_template('public/index.html', title="home")
+    return render_template('public/index.html', title="home", func=open_file())
 
 
--- run.py
+++ run.py
@@ -1,13 +1,6 @@
 from app import app
 
-stores = [
-    {
-        'name': 'Cool Store',
-        'items': [
-                {'name': 'Cool Item', 'price': 10}
-            ]
-    }
-]
 
 if __name__ == '__main__':
     app.run()
+
